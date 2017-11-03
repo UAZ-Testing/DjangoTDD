cd4619eac22c734ec7993df77d4cfe23b6d4b1b4
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2017-11-03 09:47:42 -0600

    Separa las urls de la app lists

---
+++ lists/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from django.contrib import admin
+from lists import views
+from lists import urls as list_urls
+
+urlpatterns = [
+    url(r'^new$', views.new_list, name='new_list'),
+    url(r'^(?P<list_id>\d+)/$', views.view_list, name='view_list'),
+    url(r'^(?P<list_id>\d+)/add_item$', views.add_item, name='add_item'),
+]
+
+
