45f0ed7acb3e8103dffa9497e6a1ffd2a5220701
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2017-11-04 16:01:35 -0600

    Refactor hard-coded URLs out of templates

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -28,3 +28,62 @@ class NewListTest(TestCase):
     def test_invalid_list_items_arent_saved(self):
         self.client.post('/lists/new', data={'item_text': ''})
         self.assertEqual(Item.objects.count(), 0)
+
+
+class ListViewTest(TestCase):
+    def test_passes_correct_list_to_template(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get('/lists/%d/' % (correct_list.id))
+        self.assertEqual(response.context['list'], correct_list)
+
+    def test_displays_all_items(self):
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
+
+        response = self.client.get('/lists/%d/' % (list_.id))
+
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
+
+    def test_displays_only_items_for_that_list(self):
+        correct_list = List.objects.create()
+        Item.objects.create(text='itemey 1', list=correct_list)
+        Item.objects.create(text='itemey 2', list=correct_list)
+        other_list = List.objects.create()
+        Item.objects.create(text='other list item 1', list=other_list)
+        Item.objects.create(text='other list item 2', list=other_list)
+
+        response = self.client.get('/lists/' + str(correct_list.id) + '/')
+
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
+        self.assertNotContains(response, 'other list item 1')
+        self.assertNotContains(response, 'other list item 2')
+
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            '/lists/%d/' % (correct_list.id),
+            data={'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_POST_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            '/lists/%d/' % (correct_list.id),
+            data={'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertIn('/lists/%d/' % (correct_list.id), response.url, )
+
--- lists/views.py
+++ lists/views.py
@@ -12,7 +12,18 @@ from lists.models import Item, List
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    error = request.GET.get('error', None)
+    error = request.GET.get('error', '')
+
+    if request.method == 'POST':
+        item = Item(text=request.POST['item_text'], list=list_)
+
+        try:
+            item.full_clean()
+            item.save()
+        except ValidationError:
+            error = 'The item cannot be empty'
+
+        return redirect('/lists/%d/?error=%s' % (list_.id, error))
 
     return render(request, 'list.html', {
         'list': list_,
@@ -22,7 +33,7 @@ def view_list(request, list_id):
 
 def new_list(request):
     list_ = List.objects.create()
-    item = Item(text=request.POST['item_text'], list=list_)
+    item = Item(text=request.POST.get('item_text', None), list=list_)
     error = ''
 
     try:
@@ -31,19 +42,5 @@ def new_list(request):
     except ValidationError:
         error = 'The item cannot be empty'
         return render(request, 'home.html', {"error": error})
-
-    return redirect('/lists/%d/?error=%s' % (list_.id, error))
-
-
-def add_item(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    item_text = request.POST.get('item_text', None)
-
-    error = ''
-
-    if item_text is None or len(item_text) == 0:
-        error = 'The item cannot be empty'
-    else:
-        Item.objects.create(text=request.POST['item_text'], list=list_)
 
     return redirect('/lists/%d/?error=%s' % (list_.id, error))
