6ed2f243de5576e9aeef403069f877d4baf011a7
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2017-11-06 23:19:08 -0600

    Finish chapter 15

--- lists/models.py
+++ lists/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.core.urlresolvers import reverse
 
 
-# Create your models here.
-
 class List(models.Model):
     def get_absolute_url(self):
         return reverse('view_list', args=[self.id])
@@ -13,9 +11,9 @@ class Item(models.Model):
     text = models.TextField(default='')
     list = models.ForeignKey(List, default=None)
 
-    def __str__(self):
-        return self.text
-
     class Meta:
         ordering = ('id',)
         unique_together = ('list', 'text')
+
+    def __str__(self):
+        return self.text
